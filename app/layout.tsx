15c0b86030570a0c32c0ff5c48445ae05c1ed624
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-05-11 23:10:30 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon_circular.png" type="image/png" />
       </head>
       <body className={inter.className}>
         {" "}
